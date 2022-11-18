b748cf542fe7c353c88f5f0df5cb19ea38e241f4
Filipe Ribeiro <68390743+FilipeRibeiroDev@users.noreply.github.com>
2022-11-18 07:35:10 -0300

    First commit

---
+++ AutomationDesktop/Commands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationDesktop
+{
+    public class Commands
+    {
+        public string Type { get; set; }
+
+        public dynamic Value { get; set; }
+    }
+}
